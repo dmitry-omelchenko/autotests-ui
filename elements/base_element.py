aab82c292fc2372dd09c6ea0993c3373a4a87c8a
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2025-12-29 20:47:07 +0300

    8.3 Добавление Allure шагов в автотесты. Расстановка allure.step для Page Factory

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page, Locator, expect
 
 
@@ -7,21 +8,29 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
+    @property
+    def type_of(self) -> str:
+        return "base element"
+
     # The method takes keyword arguments (kwargs)
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:  # Locator object for interacting with the element
         # Initializes the locator object by inserting dynamic values into the locator.
         locator = self.locator.format(**kwargs)
-        # Return the locator object
-        return self.page.get_by_test_id(locator).nth(nth)
+        with allure.step(f'Getting locator with "dsta-testid={locator}" at index "{nth}"'):
+            # Return the locator object
+            return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self, nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        locator.click()
+        with allure.step(f'Clicking {self.type_of} "{self.name}"'):
+            locator = self.get_locator(nth, **kwargs)
+            locator.click()
 
     def check_visible(self, nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_be_visible()
+        with allure.step(f'Checking that {self.type_of} "{self.name}" is visible'):
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_be_visible()
 
     def check_have_text(self, text: str, nth: int = 0, **kwargs):
-        locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_have_text(text)
+        with allure.step(f'Checking that {self.type_of} "{self.name}" has text "{text}"'):
+            locator = self.get_locator(nth, **kwargs)
+            expect(locator).to_have_text(text)
