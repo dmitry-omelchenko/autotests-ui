e0999cea5a428bff0b5277d5d1869f0e55cef5d0
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2025-12-29 14:38:44 +0300

    8.2 Интеграция Allure в автотесты. Allure аннотации: @allure.tag

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -4,10 +4,12 @@ import pytest
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
@@ -17,6 +19,7 @@ class TestAuthorization:
             ("  ", "password")
         ]
     )
+    @allure.tag(AllureTag.USER_LOGIN)
     @allure.title("User login with wrong email or password")
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
@@ -24,6 +27,7 @@ class TestAuthorization:
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.tag(AllureTag.USER_LOGIN)
     @allure.title("User login with correct email and password")
     def test_successful_authorization(
             self,
@@ -47,6 +51,7 @@ class TestAuthorization:
         dashboard_page.navbar.check_visible("username")
         dashboard_page.sidebar.check_visible()
 
+    @allure.tag(AllureTag.NAVIGATION)
     @allure.title("Navigation from login page to registration page")
     def test_navigate_from_authorization_to_registration(
             self,
