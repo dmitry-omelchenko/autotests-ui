49a7d0ccd590d833dae03907c49408c9ba792d8c
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2026-01-05 19:29:21 +0300

    8.5 Добавление видео в Allure отчет. Рефакторинг фикстур chromium_page и chromium_page_with_state

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,23 +4,12 @@ from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-
-    yield page
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
-
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
@@ -39,15 +28,8 @@ def initialize_browser_state(playwright: Playwright):
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json", record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-
-    yield page
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state="browser-state.json"
+    )
