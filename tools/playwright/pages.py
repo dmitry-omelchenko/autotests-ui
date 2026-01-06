9b7c309ab86e182e238a40c34c451d5bcad5dc12
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2026-01-06 13:51:48 +0300

    9.1 Настройки автотестов с Pydantic.Практика работы с настройками

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -10,7 +10,11 @@ def initialize_playwright_page(
         storage_state: str | None = None
 ) -> Page:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state=storage_state, record_video_dir='./videos')
+    context = browser.new_context(
+        base_url=settings.get_base_url(),
+        storage_state=storage_state,
+        record_video_dir='./videos'
+    )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
 
