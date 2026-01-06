5f63a06bcac0d2ecc1abece297ccbe701d264c05
dmitry-omelchenko <dmitriyomeltschenko@gmail.com>
2026-01-06 20:16:41 +0300

    9.5 Логирование взаимодействий со страницей. Практика работы с логированием

--- elements/input.py
+++ elements/input.py
@@ -2,22 +2,31 @@ import allure  # Импортируем allure
 from playwright.sync_api import Locator, expect
 
 from elements.base_element import BaseElement
+from tools.logger import get_logger
+
+logger = get_logger("INPUT")
 
 
 class Input(BaseElement):
     @property
-    def type_of(self) -> str:  # Переопределяем свойство type_of
+    def type_of(self) -> str:  # Overriding the type_of property
         return "input"
 
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
     def fill(self, value: str, nth: int = 0, **kwargs):
-        with allure.step(f'Fill {self.type_of} "{self.name}" to value "{value}"'):  # Добавили шаг
+        step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
+
+        with allure.step(step):  # Add step
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.fill(value)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" has a value "{value}"'):  # Добавили шаг
+        step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_have_value(value)
